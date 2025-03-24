7d926c4c2889d53dab73d3c964783a487586802d
djph7758-uol <djph7758@leeds.ac.uk>
2025-03-24 13:04:17 +0000

    Team M2 Data pereration file

---
+++ data_preperation.M2.py
@@ -0,0 +1,12 @@
+#1. download_answer_files(string cloud_url, string
+#path_to_data_folder, int respondent_index)
+#• Input: Cloud service URL containing files named a1.txt, a2.txt, ...,
+#an.txt.
+#• Output: Saves files to the data/ folder as answers_respondent_1.txt,
+#answers_respondent_2.txt, ..., answers_respondent_n.txt.
+#• Purpose: Retrieve and rename raw answer files from the cloud.
+#2. collate_answer_files(string data_folder_path)
+#• Input: Path to the data/ folder containing individual answer files.
+#• Output: Single file collated_answers.txt in output/, with respondent
+#sections separated by a line containing one asterisk (*).
+#• Purpose: Combine all respondent data into a unified file.
