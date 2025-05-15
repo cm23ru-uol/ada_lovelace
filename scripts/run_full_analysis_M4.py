97249b590f8c2c38abf65f67efff28fd82b60356
Richard-Ubege <richard.ubege05@gmail.com>
2025-05-15 09:29:36 +0100

    m4 15/05/25

--- scripts/run_full_analysis_M4.py
+++ scripts/run_full_analysis_M4.py
@@ -1,10 +1,29 @@
 import data_preperation_M2 as m2
 import data_extraction_M1 as m1
+import data_analysis_M3 as m3
+import os 
 
 cloud_url = "https://raw.githubusercontent.com/djph7758-uol/Data/refs/heads/main/quiz_answers_named_a1_to_a25"
 data_folder = r"C:\Users\richa_6\Data Science Leeds\MATH1604\Summative Group Project\ada_lovelace\data"
 output_folder = r"C:\Users\richa_6\Data Science Leeds\MATH1604\Summative Group Project\ada_lovelace\output"
 
-for i in range(1, 26):
+num_of_respondents = 25
+for i in range(1, num_of_respondents+1):
     m2.download_answer_file(cloud_url=cloud_url, path_to_data_folder=data_folder, respondent_index=i)
 m2.collate_answer_files(data_folder_path=data_folder, output_folder_path=output_folder)
+
+#Directory containing the collated respondents
+collated = r"C:\Users\richa_6\Data Science Leeds\MATH1604\Summative Group Project\ada_lovelace\output\collated_answers.txt"
+
+respondent_files = os.listdir(data_folder)
+
+respondent_response_data = []
+for i in respondent_files:
+    respondent_response_data.append(m1.extract_answers_sequence(f"{data_folder}\\{i}"))
+    
+# print(respondent_response_data[0])
+# print(len(respondent_response_data[0]))
+
+gms = m3.generate_means_sequence(collated)
+m3.visualize_data(collated, 1)
+# print(gms)
